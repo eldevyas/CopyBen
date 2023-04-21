ddc3bf96e19f6a5ea930aa55a68dcbd6fcb51f8c
AYMANE MADANE <AYMECD123@gmail.com>
2023-04-21 03:37:59 +0200

    OurProduct  interface

--- client/pages/products.tsx
+++ client/pages/products.tsx
@@ -7,19 +7,19 @@ export default function produits() {
     return (
         <div >
             <p>Super Promo <small>(NOS SUPERS PROMOTIONS)</small></p>
-            <div style={{display:"flex"}}>
-                <div style={{backgroundColor:"blue",float:"left",width:"400px"}}>
-                <h1>left</h1>
+            <div style={{display:"flex",flexDirection:"row",flexWrap:"wrap"}}>
+                <div style={{backgroundColor:"blue",float:"left",width:"30%"}}>
+                    <h1>left</h1>
                 </div>
-            <div style={{width:"600px"}}>
-            <h1>center</h1>
-            <label></label>
-            <input type="text" />
-            </div>
-            <div style={{backgroundColor:"black",float:"right",width:"400px",color:"white"}}>
-            <h1>right</h1>
-            </div>
-            </div>
+                <div style={{width:"40%"}}>
+                    <h1>center</h1>
+                    <label></label>
+                    <input type="text" />
+                </div>
+                <div style={{backgroundColor:"black",width:"30%",color:"white"}}>
+                    <h1>right</h1>
+                </div>
+                </div>
         </div>);
        
        
