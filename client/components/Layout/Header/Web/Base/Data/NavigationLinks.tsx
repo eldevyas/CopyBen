1bd826ff37948d53a9619f174406c67cfbe582e9
Yassine Chettouch <95131504+yassine-ct@users.noreply.github.com>
2023-04-22 20:47:13 +0000

    ⚡Web Header Complete

---
+++ client/components/Layout/Header/Web/Base/Data/NavigationLinks.tsx
@@ -0,0 +1,55 @@
+let MenuItems: {
+    Name: string;
+    Link: string;
+}[] = [
+    {
+        Name: "CARTES DE VISITE",
+        Link: "/",
+    },
+    {
+        Name: "FLYERS",
+        Link: "/",
+    },
+    {
+        Name: "DÉPLIANTS",
+        Link: "/",
+    },
+    {
+        Name: "BROCHURES",
+        Link: "/",
+    },
+    {
+        Name: "AFFICHES",
+        Link: "/",
+    },
+    {
+        Name: "CHEMISES",
+        Link: "/",
+    },
+    {
+        Name: "CHEMISE A RABAT",
+        Link: "/",
+    },
+    {
+        Name: "BLOC NOTE",
+        Link: "/",
+    },
+    {
+        Name: "CALENDRIER",
+        Link: "/",
+    },
+    {
+        Name: "LETTRES À EN TÊTE",
+        Link: "/",
+    },
+    {
+        Name: "ENVELOPPE",
+        Link: "/",
+    },
+    {
+        Name: "CARNET DE TICKETS",
+        Link: "/",
+    },
+];
+
+export default MenuItems;
--- client/components/Layout/Header/Web/WebHeader.tsx
+++ client/components/Layout/Header/Web/WebHeader.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import TopBar from "./Base/1. Top Bar";
+import Main from "./Base/2. Main";
+import Navigation from "./Base/3. Navigation";
 
 export default function WebHeader() {
     return (
         <div className="Header WebHeader">
             <TopBar />
-            <div className="Header__Main"></div>
-            <div className="Header__Navigation"></div>
+            <Main />
+            <Navigation />
         </div>
     );
 }
