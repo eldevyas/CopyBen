4d83050b9f3baa728103a4ffaeb38646cf98454f
Yassine Chettouch <95131504+yassine-ct@users.noreply.github.com>
2023-05-02 13:23:10 +0100

    Useless Modules Removed

--- server/routes/api.php
+++ server/routes/api.php
@@ -18,5 +18,6 @@ use App\Http\Controllers\UploadController;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
 Route::post('/upload', [UploadController::class, 'store']);
 Route::post('/form', [UploadController::class, 'send_form']);
