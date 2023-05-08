d9404c2ca6326c562a656d1c632bfb4023c32044
Yassine Chettouch <95131504+yassine-ct@users.noreply.github.com>
2023-05-08 03:14:59 +0100

    hhh

---
+++ client/pages/produits/flyers.tsx
@@ -0,0 +1,31 @@
+import { GetStaticPaths, GetStaticProps } from "next";
+import { Product } from "@/types/Product";
+import ProductPage from "@/components/Pages/Products/ProductPage";
+import Products, { getProductByName } from "@/data/Products";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+import { useAuth } from "@/context/AuthContext";
+
+interface ProductPageProps {
+    Product: Product;
+}
+
+const ProductPageRoute = () => {
+    const Router = useRouter();
+    const { isLoggedIn }: any = useAuth();
+
+    useEffect(() => {
+        const checkAuthentication = () => {
+            if (!isLoggedIn) {
+                // Redirect to the homepage or a fallback URL
+                Router.push("/auth/login");
+            }
+        };
+
+        checkAuthentication();
+    }, [Router, isLoggedIn]);
+
+    return <ProductPage Product={Products[1]} />;
+};
+
+export default ProductPageRoute;
